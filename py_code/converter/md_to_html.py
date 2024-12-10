434c0f1c1be68815f21328596efc5416ffaf924b
josssss07 <joshuadesai03@gmail.com>
2024-12-10 23:57:33 +0530

    py code added

--- py_code/converter/md_to_html.py
+++ py_code/converter/md_to_html.py
@@ -3,14 +3,12 @@ import sys
 
 def convert_markdown_to_html(input_file, output_file):
     try:
-        # Read the Markdown file
+   
         with open(input_file, 'r', encoding='utf-8') as md_file:
             markdown_content = md_file.read()
 
-        # Convert Markdown to HTML
         html_content = markdown.markdown(markdown_content)
 
-        # Write the HTML content to the output file
         with open(output_file, 'w', encoding='utf-8') as html_file:
             html_file.write(html_content)
 
@@ -22,7 +20,6 @@ def convert_markdown_to_html(input_file, output_file):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments are passed
     if len(sys.argv) != 3:
         print("Usage: python md_to_html.py <input_markdown_file> <output_html_file>")
     else:
